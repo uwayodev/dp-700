f777ff4f43a5d603f3adc815ea46efcb3405a582
uwayodev <118353145+uwayodev@users.noreply.github.com>
2026-01-12 21:55:31 +0200

    this is a super cool feature

---
+++ e021/np_dp700_e021_feature.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+print("feature")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
